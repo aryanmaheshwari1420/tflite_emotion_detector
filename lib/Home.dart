37222a1b54813dde181265b2f11f1525f3fe4ec2
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-10-23 00:26:08 +0530

    changes

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,16 +2,99 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:tflite/tflite.dart';
+import 'package:tflite_audio/tflite_audio.dart';
+import 'package:tflite_emotion_detector/main.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   CameraImage? cameraImage;
-  const HomeScreen({Key? key}) : super(key: key);
+
+  CameraController? cameraController;
+
+  String output = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    loadCamera();
+    loadModel();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text("tflite emotion detector"),
-    ));
+      appBar: AppBar(
+        title: const Text("tflite emotion detector"),
+      ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: !cameraController!.value.isInitialized
+                  ? Container()
+                  : AspectRatio(
+                      aspectRatio: cameraController!.value.aspectRatio,
+                      child: CameraPreview(cameraController!),
+                    )),
+        ),
+        Text(
+          output,
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        )
+      ]),
+    );
+  }
+
+  // functions
+
+  void runModel() async {
+    if (cameraImage != null) {
+      var predict = await Tflite.runModelOnFrame(
+        bytesList: cameraImage!.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
+    }
+  }
+
+  void loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model.tflite", labels: "assets/labels.txt");
+  }
+
+  void loadCamera() {
+    cameraController = CameraController(camera![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((imageStream) {
+            cameraImage = imageStream;
+            runModel();
+          });
+        });
+      }
+    });
   }
 }
